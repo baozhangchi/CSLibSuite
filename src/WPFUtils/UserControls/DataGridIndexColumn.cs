b41f853d262913ca66c84efc8c9b04d82bc19640
包张驰 <baozhangchi@live.com>
2022-04-18 17:40:26 +0800

    修复BUG

--- src/WPFUtils/UserControls/DataGridIndexColumn.cs
+++ src/WPFUtils/UserControls/DataGridIndexColumn.cs
@@ -11,19 +11,31 @@ namespace WPFUtils.UserControls
 {
     public class DataGridIndexColumn : DataGridColumn
     {
+        public DataGridIndexColumn()
+        {
+            IsReadOnly = true;
+            Header = "序号";
+        }
+
         protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            return GenerateContent(cell, dataItem);
+        }
+
+        private FrameworkElement GenerateContent(DataGridCell cell, object dataItem)
         {
             var textBlock = new TextBlock();
             if (cell.DataContext is IList list)
             {
                 textBlock.Text = $"{list.IndexOf(dataItem) + 1}";
             }
-            throw new NotImplementedException();
+
+            return textBlock;
         }
 
         protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
         {
-            throw new NotImplementedException();
+            return GenerateContent(cell, dataItem);
         }
     }
 }
